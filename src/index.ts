4a3b7e1ddb9347c4fc6f44f54e5d60d2631a15b3
Hewon Jeong <hewon.jng@gmail.com>
2018-11-06 20:33:19 +0900

    add get session page for test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
+import dotenv from 'dotenv'
 import puppeteer from 'puppeteer'
+dotenv.config()
+interface Auth {
+  username: string
+  password: string
+}
+interface Selecor extends Auth {
+  submit: string
+}
 
-async function main() {
+async function getSession(params: {
+  signinPage: string
+  auth: Auth
+  selector: Selecor
+}) {
+  const { signinPage, auth, selector } = params
   const browser = await puppeteer.launch()
 
   const page = await browser.newPage()
-  await page.setViewport({ width: 1440, height: 800 })
-  await page.goto('https://google.com')
-
-  console.log(await page.content())
+  await page.setViewport({ width: 1480, height: 800 })
+  console.log('signinPage', signinPage)
+  await page.goto(signinPage)
+  await page.evaluate(
+    (auth, selector) => {
+      document.querySelector(selector.username).value = auth.username
+      document.querySelector(selector.password).value = auth.password
+      document.querySelector(selector.submit).click()
+    },
+    auth,
+    selector
+  )
+  //const cookies = await page.cookies()
+  //console.log('cookies', cookies)
+  await page.goto('http://rgo4.com/hot')
   await page.screenshot({ path: 'screenshot.png' })
-  await browser.close()
 }
 
-main()
+const { USERNAME = '', PASSWORD = '', SIGNIN_PAGE = '' } = process.env
+const {
+  USERNAME_SELECTOR = '',
+  PASSWORD_SELECTOR = '',
+  SUBMIT_SELECTOR = '',
+} = process.env
+const params = {
+  signinPage: SIGNIN_PAGE,
+  auth: { username: USERNAME, password: PASSWORD },
+  selector: {
+    username: USERNAME_SELECTOR,
+    password: PASSWORD_SELECTOR,
+    submit: SUBMIT_SELECTOR,
+  },
+}
+
+getSession(params)
