aa05486cbb99f49844e3b3ccf75f22e635031337
Hewon Jeong <hewon.jng@gmail.com>
2018-11-06 12:35:11 +0900

    feat: add puppeteer to take a screenshot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-export class Greeter {
-  private greeting: string
-  constructor(message: string) {
-    this.greeting = message
-  }
+import puppeteer from 'puppeteer'
 
-  greet = () => `🖖 Hello, ${this.greeting}`
+async function main() {
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1440, height: 800 })
+  await page.goto('https://google.com')
+
+  console.log(await page.content())
+  await page.screenshot({ path: 'screenshot.png' })
+  await browser.close()
 }
 
-const greeter = new Greeter('world')
-console.log(greeter.greet())
+main()
