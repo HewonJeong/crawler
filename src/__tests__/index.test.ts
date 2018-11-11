e5e8e5bc19ae0c23ff625ad5e6a841f94bcc52b4
Hewon Jeong <hewon.jng@gmail.com>
2018-11-11 11:01:13 +0900

    write simple index.ts

--- src/__tests__/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { Greeter } from '..'
-
-test('basic', () => {
-  const g = new Greeter('world')
-  expect(g.greet()).toEqual('🖖 Hello, world')
-})
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,11 @@
 import dotenv from 'dotenv'
-import puppeteer from 'puppeteer'
-dotenv.config()
-interface Auth {
-  username: string
-  password: string
-}
-interface Selecor extends Auth {
-  submit: string
-}
+import PostsService from './PostsService'
+import Post, { PricePost } from './Post'
+dotenv.config({ path: '.env.local' })
 
-async function getSession(params: {
-  signinPage: string
-  auth: Auth
-  selector: Selecor
-}) {
-  const { signinPage, auth, selector } = params
-  const browser = await puppeteer.launch()
-
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1480, height: 800 })
-  console.log('signinPage', signinPage)
-  await page.goto(signinPage)
-  await page.evaluate(
-    (auth, selector) => {
-      document.querySelector(selector.username).value = auth.username
-      document.querySelector(selector.password).value = auth.password
-      document.querySelector(selector.submit).click()
-    },
-    auth,
-    selector
-  )
-  //const cookies = await page.cookies()
-  //console.log('cookies', cookies)
-  await page.goto('http://rgo4.com/hot')
-  await page.screenshot({ path: 'screenshot.png' })
+async function main() {
+  const isTarget = (p: Post) => p instanceof PricePost
+  const posts = (await PostsService.getPosts()).filter(isTarget)
+  console.log(posts)
 }
-
-const { USERNAME = '', PASSWORD = '', SIGNIN_PAGE = '' } = process.env
-const {
-  USERNAME_SELECTOR = '',
-  PASSWORD_SELECTOR = '',
-  SUBMIT_SELECTOR = '',
-} = process.env
-const params = {
-  signinPage: SIGNIN_PAGE,
-  auth: { username: USERNAME, password: PASSWORD },
-  selector: {
-    username: USERNAME_SELECTOR,
-    password: PASSWORD_SELECTOR,
-    submit: SUBMIT_SELECTOR,
-  },
-}
-
-getSession(params)
+main()
