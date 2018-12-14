f9bd4dcf60121d8dd82096e4058510bd04581fac
Hewon Jeong <hewon.jng@gmail.com>
2018-12-14 21:35:26 +0900

    do not anything if there is no target post

--- src/scrapper/index.ts
+++ src/scrapper/index.ts
@@ -10,6 +10,7 @@ export default async function scrap() {
   const date = moment().format('YYYY-MM-DD')
   const isTarget = (p: Post) => p instanceof PricePost && p.date === date
   const posts = (await PostsService.getPosts()).filter(isTarget)
+  if (posts.length === 0) return
   const content = await PostsService.getContent(posts[0].link)
   const output = `${date}.html`
   const key = await StorageService.store(output, buildHtml(date, content))
